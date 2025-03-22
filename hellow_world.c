bfea2f5ebbaf274307fc069c594f9f6b58c33e71
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-03-22 21:34:03 +0500

    write my first c program

---
+++ hellow_world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* Include this standard input output library */
+int main()
+/* main function to run c code  */
+{
+    printf("Hello World\n");
+    /* Print hello world to the screen \ is an escape sequence followed by n it means new line */
+    return 0 ;
+    /* exit the program */
+}
